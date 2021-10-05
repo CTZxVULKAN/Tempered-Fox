5eed5a36c5e1b2629527575bde50fb2f34ddbb8e
VULKAN <65299153+CTZxVULKAN@users.noreply.github.com>
2021-10-05 03:34:52 +0000

    Update user.js

--- user.js
+++ user.js
@@ -2,27 +2,28 @@
 //  SECTION : COOKIES AND HISTORY
 // -------------------------------------------
 
-
+// Enable strict mode
 defaultPref("network.cookie.cookieBehavior", 5); // dFPI, same as strict mode
+
+// Delete cookies on exit
 defaultPref("network.cookie.lifetimePolicy", 2); // keep cookies until the browser is closed then delete everything minus exceptions
 
-// make third party and http cookies session-only
+// Make third party and http cookies session-only
 defaultPref("network.cookie.thirdparty.sessionOnly", true);
 defaultPref("network.cookie.thirdparty.nonsecureSessionOnly", true);
 
-/**
- This way of sanitizing would override the exceptions set by the users and just delete everything,
- therefore we tell it to delete everything but ignore data needed to stay logged into websites set
- manually as exceptions.
-*/
-defaultPref("privacy.clearOnShutdown.cookies", false);
-defaultPref("privacy.clearOnShutdown.offlineApps", false);
-defaultPref("privacy.sanitize.sanitizeOnShutdown", true);
+// Increase time between session saves
+defaultPref("browser.sessionstore.privacy_level", 2);
+defaultPref("browser.sessionstore.interval", 60000); // prevent websites from storing session data like cookies and forms by increasing time between session saves
 
 // disable browsing, search and form history
 defaultPref("places.history.enabled", false);
 defaultPref("browser.formfill.enable", false);
 
-// prevent websites from storing session data like cookies and forms, increase time between session saves
-defaultPref("browser.sessionstore.privacy_level", 2);
-defaultPref("browser.sessionstore.interval", 60000);
+// Delete everything in history on exit, except saved login info, site settings and user defined exceptions.
+defaultPref("privacy.clearOnShutdown.cookies", false);
+defaultPref("privacy.clearOnShutdown.siteSettings", false);
+defaultPref("privacy.clearOnShutdown.offlineApps", true);
+defaultPref("privacy.sanitize.sanitizeOnShutdown", true);
+
+
